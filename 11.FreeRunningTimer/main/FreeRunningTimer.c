5c55844bfa548db3a48598c177224c86919d4c75
Pushpendra-Darky <pushpendra.assassin@gmail.com>
2022-08-21 23:39:21 +0530

    Free Running Timer Program

---
+++ 11.FreeRunningTimer/main/FreeRunningTimer.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+
+TaskHandle_t serial_task_handle;
+TimerHandle_t serial_timer_handle;
+
+void serial_timer_callback(TimerHandle_t xTimer)
+{
+    printf("Timer Fired\n");
+}
+
+
+void serial_task(void *data)
+{
+    printf("Starting The Serial Task\n");
+    
+    printf("Creating The Timer\n");
+    serial_timer_handle = xTimerCreate("FreeRunnigTimer",5000/portTICK_PERIOD_MS,pdTRUE,0,serial_timer_callback);
+    printf("Starting The Timer\n");
+    xTimerStart(serial_timer_handle,0);
+    vTaskDelete(serial_task_handle);
+    printf("Serial Task Dead\n");
+}
+
+
+void app_main()
+{
+    BaseType_t res;
+    printf("MT: Free Running Timer Project\n");
+    printf("MT: Tasks Created Successfully\n");
+    res = xTaskCreate(serial_task,"SERIALTASK",2048,NULL,5,&serial_task_handle); 
+    if (res != pdPASS)
+    {
+        printf("Task Not Created\n");
+    }
+}
